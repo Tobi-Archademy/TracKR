bfacd48a0b1996d271e905d22bc6b3bf4967e3de
Oluwatobi <oluwatobi@tractionapps.co>
2023-04-29 04:40:01 +0100

    TrackR

---
+++ app/src/main/java/com/oluwatobi/trackr/db/RunDao.kt
@@ -0,0 +1,45 @@
+package com.oluwatobi.trackr.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RunDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(run: Run)
+
+    @Delete
+    suspend fun delete(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timeStamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurnt DESC")
+    fun getAllRunsSortedByCaloriesBurnt(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurnt) FROM running_table")
+    fun getTotalCaloriesBurnt(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
+}
